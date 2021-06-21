7517d27ad710aee5a6d9e711c5cb10fae0e93d7e
heinnssin <heinnssinp@gmail.com>
2021-06-20 23:18:31 -0400

    backend-complete

--- dealers_choice_react/db.js
+++ dealers_choice_react/db.js
@@ -1,25 +1,16 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost/dealership', {
+const db = new Sequelize('postgres://localhost/dealership',{
   logging: false
-})
+} )
 
 const faker = require('faker')
 const { STRING, UUID, UUIDV4, INTEGER, DATEONLY } = require('sequelize')
-const { string } = require('prop-types')
 
 
 const Dealer = db.define('dealer', {
-  id: {
-    type: UUID,
-    defaultValue: UUIDV4,
-    primaryKey: true
-  }, 
   name: {
     type: STRING,
     allowNull: false,
-  },
-  address: {
-    type: STRING
   },
   inventoryAmount: {
     type: INTEGER
@@ -42,14 +33,19 @@ const Car = db.define('car', {
   price: {
     type: INTEGER,
     allowNull: false
-  }
+  }, 
 })
 
 const Client = db.define('client', {
+  id: {
+    type: UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true
+  },
   name: STRING,
-  address: STRING,
   age: INTEGER,
   email: {
+    type: STRING,
     isEmail: true
   }, 
   purchaseDate: DATEONLY
@@ -65,40 +61,56 @@ Dealer.hasMany(Client)
 
 
 const syncAndSeed = async() =>{
-  await db.sync( { force: true } );
+  // await db.sync( { force: true } );
 
-  const carvana = await Dealer.create({
+  const foreigns = await Dealer.create({
     name: 'FOREIGNS',
-    address: faker.address(),
+    address: faker.address.city(),
+    inventoryAmount: 10
+  })
+  const exotics = await Dealer.create({
+    name: 'Exotics',
+    address: '100 Park Ave',
     inventoryAmount: 10
   })
 
+  const [client1, client2, client3, client4]  = await Promise.all([
+    Client.create({name: faker.name.findName(), age: (18 + Math.floor(Math.random()*(55-18))), email: faker.internet.email(), purchaseDate: faker.date.past(), dealerId: (1 + Math.floor(Math.random()*(3-1)))  }),
+    Client.create({name: faker.name.findName(), age: (18 + Math.floor(Math.random()*(55-18))), email: faker.internet.email(), purchaseDate: faker.date.past(), dealerId: (1 + Math.floor(Math.random()*(3-1)))  }),
+    Client.create({name: faker.name.findName(), age: (18 + Math.floor(Math.random()*(55-18))), email: faker.internet.email(), purchaseDate: faker.date.past(), dealerId: (1 + Math.floor(Math.random()*(3-1)))  }),
+    Client.create({name: faker.name.findName(), age: (18 + Math.floor(Math.random()*(55-18))), email: faker.internet.email(), purchaseDate: faker.date.past(), dealerId: (1 + Math.floor(Math.random()*(3-1)))  }),
+  ]) 
+
   const [mcLaren, saleen, ferrari, porsche, mercedes, maserati, 
     koenigsegg, lamborghini, astonMartin, bugatti] = await Promise.all([
-      Car.create({ brand: 'McLaren', model: 'McLaren P1', type: 'Supercar', price: 100000 }),
-      Car.create({ brand: 'Saleen', model: 'Saleen S7', type: 'Supercar', price: 200000 }),
-      Car.create({ brand: 'Ferrari', model: 'LaFerrari', type: 'Supercar', price: 3000000 }),
-      Car.create({ brand: 'Porsche', model: '918 Spyder', type: 'Supercar', price: 400000 }),
-      Car.create({ brand: 'Mercedes-Benz', model: 'SLR', type: 'Supercar', price: 150000 }),
-      Car.create({ brand: 'maserati', model: 'MC12', type: 'Supercar', price: 250000 }),
-      Car.create({ brand: 'Koenigsegg', model: 'Regera', type: 'Supercar', price: 1000000 }),
-      Car.create({ brand: 'Lamborghini', model: 'Reventon', type: 'Supercar', price: 350000 }),
-      Car.create({ brand: 'Aston Martin', model: 'One-77', type: 'Supercar', price: 400000 }),
-      Car.create({ brand: 'Bugatti', model: 'Veyron Super Sport', type: 'Supercar', price: 200000 }),
+      Car.create({ brand: 'McLaren', model: 'McLaren P1', type: 'Supercar', price: 100000,  dealerId: 1, clientId: client1.id}),
+      Car.create({ brand: 'Saleen', model: 'Saleen S7', type: 'Supercar', price: 200000, dealerId: 1, clientId: client2.id}),
+      Car.create({ brand: 'Ferrari', model: 'LaFerrari', type: 'Supercar', price: 3000000, dealerId: 1, clientId: client3.id}),
+      Car.create({ brand: 'Porsche', model: '918 Spyder', type: 'Supercar', price: 400000, dealerId: 1, clientId: client4.id}),
+      Car.create({ brand: 'Mercedes-Benz', model: 'SLR', type: 'Supercar', price: 150000, dealerId: 1, clientId: client4.id }),
+      Car.create({ brand: 'maserati', model: 'MC12', type: 'Supercar', price: 250000, dealerId: 1, clientId: client1.id}),
+      Car.create({ brand: 'Koenigsegg', model: 'Regera', type: 'Supercar', price: 1000000, dealerId: 1, clientId: client2.id}),
+      Car.create({ brand: 'Lamborghini', model: 'Reventon', type: 'Supercar', price: 350000, dealerId: 1, clientId: client3.id }),
+      Car.create({ brand: 'Aston Martin', model: 'One-77', type: 'Supercar', price: 400000, dealerId: 1, clientId: client3.id }),
+      Car.create({ brand: 'Bugatti', model: 'Veyron Super Sport', type: 'Supercar', price: 200000, dealerId: 1, clientId: client3.id}),
     ])
-    for (let i = 0; i < 10; i++){
-      const newClient = await Client.create({
-        name: faker.name.findName(),
-        age: faker.age(),
-        address: faker.address(),
-        email: faker.email(),
-        purchaseDate: faker.date.past(),
-      })
-    }
+
+    
+
+    // for (let i = 0; i < 10; i++){
+    //   const newClient = await Client.create({
+    //     name: faker.name.findName(),
+    //     age: (18 + Math.floor(Math.random()*(55-18))),
+    //     email: faker.internet.email(),
+    //     purchaseDate: faker.date.past(),
+    //     dealerId: (1 + Math.floor(Math.random()*(3-1)))
+    //   })
+    // }
 }
 
 module.exports = {
   syncAndSeed, 
+  db,
   models: {
     Dealer, 
     Car, 
--- dealers_choice_react/webpack.config.js
+++ dealers_choice_react/webpack.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  entry: ['./src/index.js'],
+  mode: 'development',
   module: {
     rules: [
       {
